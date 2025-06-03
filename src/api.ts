8ffc2050227cda3ff406982c4b83377af6e0271d
gnvvoo <t412@naver.com>
2025-06-03 16:52:38 +0900

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://10.111.20.75:8080',  // ← 여기만 복사하신 IPv4 주소로 변경하세요
+  baseURL: 'http://192.168.33.252:8080',  // ← 여기만 복사하신 IPv4 주소로 변경하세요
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 });
